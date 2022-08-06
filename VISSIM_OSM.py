e1641ae07e2ac3498f2cdfcdc7e44f91bd8a8487
BME Road Traffic Control Laboratory <91676553+bmetrafficlab@users.noreply.github.com>
2022-08-06 02:52:25 +0200

    Update VISSIM_OSM.py

--- VISSIM_OSM.py
+++ VISSIM_OSM.py
@@ -29,22 +29,23 @@ file_canvas.create_window(125, 100, window=file_entry)
 
 file_entry.insert(0,'BME')
 
-def Set_osm_file():
+def Set_osm_file(e):
     
     global osm_file
     
-    osm_file=file_entry.get()+'.osm'
+    osm_file=file_entry.get()
     
     file_window.destroy()
+
+file_window.bind('<Return>', Set_osm_file)
 
 file_button=tk.Button(file_window, text='Set', command=Set_osm_file, font=('helvetica', 9, 'bold'), bg='brown', fg='white')
 file_canvas.create_window(125, 150, window=file_button)
 
 file_window.mainloop()
 
-
 #Create base .inpx XML file
-f= open("test.inpx","w+")
+f= open(str(osm_file)+".osm.inpx","w+")
 f.write("<network>\n</network>")
 f.close()
 
@@ -53,7 +54,7 @@ Vissim= com.Dispatch("Vissim.Vissim")
 
 #Get Vissim version for XML file
 
-Filename = os.path.join(os.getcwd(),"test.inpx") # in Current Working Directory 
+Filename = os.path.join(os.getcwd(),str(osm_file)+".osm.inpx") # in Current Working Directory 
 Vissim.LoadNet(Filename, True)
 
 # ----Base data------
@@ -72,9 +73,9 @@ Vissim.Net.VehicleClasses.AddVehicleClass(1)
 # osm_file='BME.osm'
 
 driver=ogr.GetDriverByName('OSM')
-data_source = driver.Open(osm_file)
+data_source = driver.Open(osm_file+'.osm')
 
-tree = ET.parse(osm_file)
+tree = ET.parse(osm_file+'.osm')
 root = tree.getroot()
 
 no_turn_restrictions=[]
@@ -82,40 +83,20 @@ only_restrictions=[]
 
 SH_ID=1
 
-g=open('JSON.txt', 'w').close()
-g=open('JSON.txt', 'a')
-
-datatypes=['points', 'lines', 'multilinestrings', 'multipolygons', 'other_relations']
-
-for datatype in datatypes:
-    
-    layer = data_source.GetLayer(datatype)
-    
-    features=[x for x in layer]
-
-    for feature in features:
-        
-        data=feature.ExportToJson(as_object=True)
-        
-        try:
-        
-            g.write(str(data)+'\n')
-        
-        except:
-            
-            continue
-
-g.close()
-
 layer = data_source.GetLayer('lines')
 
 features=[x for x in layer]
 
+short_links=0
 Links=[]
 link_key=1
 carways=[]
 footways=[]
 cycleways=[]
+all_cycleway_coords={}
+all_carway_coords={}
+carways_with_crossing=[]
+cycleways_with_crossing=[]
 
 print('Creating links')
 
@@ -136,8 +117,8 @@ for feature in features:
     other_tags=data['properties']['other_tags']
     name=data['properties']['name']
     highway=data['properties']['highway']
-    highways=['footway','cycleway','motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified', 'residental', 'motoway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link', 'service', 'track', 'bus_guideway', 'escape', 'raceway', 'road', 'busway', 'living_street', 'residential']
-
+    highways=['steps','footway','cycleway','motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified', 'residental', 'motoway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link', 'service', 'track', 'bus_guideway', 'escape', 'raceway', 'road', 'busway', 'living_street', 'residential']
+    
     if other_tags and ('"lanes:forward"' in other_tags or '"lanes:backward"' in other_tags or '"turn:lanes:forward"' in other_tags or '"turn:lanes:backward"' in other_tags):
         
         if '"turn:lanes:forward"' in other_tags:
@@ -259,7 +240,7 @@ for feature in features:
             
             width=1.5
 
-        elif highway=='footway':
+        elif highway=='footway' or highway=='steps':
             
             width=1
 
@@ -290,7 +271,7 @@ for feature in features:
                     width=3.5
                     
                 lanes=0
-                
+
         elif other_tags and not('"oneway"' in other_tags):
             
             if '"junction"' in other_tags:
@@ -323,6 +304,43 @@ for feature in features:
             width=3.5
             
         widths=[]
+
+        if width==1.5:
+            
+            key_list=list(all_cycleway_coords.keys())
+            value_list=list(all_cycleway_coords.values())
+
+            for way_1_coords in coords:
+                
+                for way_2_coords in value_list:
+                    
+                    if way_1_coords in way_2_coords:
+
+                        cycleways_with_crossing.append([str(way_id), str(key_list[value_list.index(way_2_coords)])])
+                        cycleways_with_crossing.append(['-'+str(way_id), str(key_list[value_list.index(way_2_coords)])])
+                        cycleways_with_crossing.append([str(way_id), '-'+str(key_list[value_list.index(way_2_coords)])])
+                        cycleways_with_crossing.append(['-'+str(way_id), '-'+str(key_list[value_list.index(way_2_coords)])])
+
+            all_cycleway_coords[way_id]=coords
+            
+        elif width==3.5 or width==2.75:
+            
+            key_list=list(all_carway_coords.keys())
+            value_list=list(all_carway_coords.values())
+
+            for way_1_coords in coords:
+                
+                for way_2_coords in value_list:
+                    
+                    if way_1_coords in way_2_coords:
+
+                        carways_with_crossing.append([str(way_id), str(key_list[value_list.index(way_2_coords)])])
+                        carways_with_crossing.append(['-'+str(way_id), str(key_list[value_list.index(way_2_coords)])])
+                        carways_with_crossing.append([str(way_id), '-'+str(key_list[value_list.index(way_2_coords)])])
+                        carways_with_crossing.append(['-'+str(way_id), '-'+str(key_list[value_list.index(way_2_coords)])])
+     
+            all_carway_coords[way_id]=coords
+                
         
         if lanes_forward==0:
         
@@ -360,17 +378,16 @@ for feature in features:
             Vissim.Net.Links.AddLink(way_id, str(sg.LineString(linestring)), widths)
             )
         
-                    
-        if Links[-1].AttValue('Length2D')<5:
-            
-            Vissim.Net.Links.RemoveLink(Links[-1])
-            
-            del Links[-1]
-            
-            continue
-        
         Links[-1].SetAttValue('Name', way_id)
         
+        # if Links[-1].AttValue('Length2D')<5:
+            
+        #     Vissim.Net.Links.RemoveLink(Links[-1])
+            
+        #     del Links[-1]
+            
+        #     continue
+
         if width==3.5 or width==2.75:
             
             carways.append(Links[-1])
@@ -407,75 +424,107 @@ for feature in features:
         
         link_key=link_key+1
 
+for link_no in range(short_links):
+    
+    Vissim.Net.Links.RemoveLink(Vissim.Net.Links.ItemByKey(link_no+1))
+
 def Create_connectors(way_type):
     
     if way_type==carways:
+  
+        way_2_list=carways_with_crossing
 
         print('Creating carway connectors')
         
     elif way_type==cycleways:
         
+        way_2_list=cycleways_with_crossing
+        
         print('Creating cycleway connectors')
     
     serial_number=0
     
+    percentages=[]
+    
     for way_1 in way_type:
-        
-        if serial_number%5==0:
+
+        percentage=serial_number/len(way_type)*100
+
+        if int(percentage)%10==0 and not(int(percentage) in percentages):
             
-            percentage=serial_number/len(way_type)*100
+            percentages.append(int(percentage))
             
-            print("%.2f" % percentage)
+            print("%.2f" % int(percentage)+"%")
             
         serial_number=serial_number+1
         
-        for restriction in only_restrictions:
-            
-            if restriction[0]==int(way_1.AttValue('Name')):
+        if only_restrictions!=[]:
+        
+            for restriction in only_restrictions:
                 
-                has_rest=True
-
-                break
+                if restriction[0]==int(way_1.AttValue('Name')):
+                    
+                    has_rest=True
+    
+                    break
+                
+                else:
+    
+                    has_rest=False
+                    
+        else:
             
-            else:
-
-                has_rest=False
+            has_rest=False
 
         linkpolypts_1=way_1.LinkPolyPts.GetAll()
         
         coord_x_1=linkpolypts_1[-1].AttValue('X')
         coord_y_1=linkpolypts_1[-1].AttValue('Y')
-
+        
+        way_1_start=[linkpolypts_1[0].AttValue('X'), linkpolypts_1[0].AttValue('Y')]
+        
         for way_2 in way_type:             
+            
+            if way_2.AttValue('Length2D')<=5 or way_1.AttValue('Length2D')<=5:
+                
+                dist=3
+                
+            else:
+                
+                dist=5
+
+            if [way_1.AttValue('Name'), way_2.AttValue('Name')] in way_2_list or [way_2.AttValue('Name'), way_1.AttValue('Name')] in way_2_list:
+
+                pass
+            
+            else:
+                
+                continue
 
             if has_rest and not([int(way_1.AttValue('Name')),int(way_2.AttValue('Name'))] in only_restrictions):
     
                 continue
+
+            # way_2_start=[way_2.LinkPolyPts.GetAll()[0].AttValue('X'),way_2.LinkPolyPts.GetAll()[0].AttValue('Y')]
             
-            way_2_start=[way_2.LinkPolyPts.GetAll()[0].AttValue('X'),way_2.LinkPolyPts.GetAll()[0].AttValue('Y')]
-            
-            way_2_end=[way_2.LinkPolyPts.GetAll()[-1].AttValue('X'),way_2.LinkPolyPts.GetAll()[-1].AttValue('Y')]
-            
-            if math.dist([coord_x_1, coord_y_1],way_2_start)>way_2.AttValue('Length2D') and math.dist([coord_x_1, coord_y_1],way_2_end)>way_2.AttValue('Length2D'):
-                
-                continue
-            
-            if len(way_2.LinkPolyPts.GetAll())>2:
+            # way_2_end=[way_2.LinkPolyPts.GetAll()[-1].AttValue('X'),way_2.LinkPolyPts.GetAll()[-1].AttValue('Y')]
+
+            # if len(way_2.LinkPolyPts.GetAll())>2:
                    
-                cycle_length=len(way_2.LinkPolyPts.GetAll())-2
+            #     cycle_length=len(way_2.LinkPolyPts.GetAll())-1
                                     
-            else:
+            # else:
         
-                cycle_length=1    
+            #     cycle_length=1    
         
-            for linkpoly_2_index in range(cycle_length):
+            for linkpoly_2_index in range(len(way_2.LinkPolyPts.GetAll())):
 
                 linkpoly_2=way_2.LinkPolyPts.GetAll()[linkpoly_2_index]
                 
                 coord_x_2=linkpoly_2.AttValue('X')
                 coord_y_2=linkpoly_2.AttValue('Y')
-       
-                if way_1!=way_2 and int(way_1.AttValue('Name'))!=-int(way_2.AttValue('Name')) and not([int(way_1.AttValue('Name')),int(way_2.AttValue('Name'))] in no_turn_restrictions) and math.dist([coord_x_1, coord_y_1], [coord_x_2, coord_y_2])<5:
+
+                if linkpoly_2_index!=len(way_2.LinkPolyPts.GetAll())-1 and way_1!=way_2 and int(way_1.AttValue('Name'))!=-int(way_2.AttValue('Name')) and not([int(way_1.AttValue('Name')),int(way_2.AttValue('Name'))] in no_turn_restrictions) and math.dist([coord_x_1, coord_y_1], [coord_x_2, coord_y_2])<dist:
 
                     polypoints=way_2.LinkPolyPts.GetAll()
                     
@@ -505,13 +554,13 @@ def Create_connectors(way_type):
                         lane_connection_1=way_1.AttValue('NumLanes')-lanes+1
                         lane_connection_2=way_2.AttValue('NumLanes')-lanes+1
                     
-                    if linkpoly_2_index!=0 and linkpoly_2_index!=cycle_length-1:
+                    if linkpoly_2_index!=0 and linkpoly_2_index!=len(way_2.LinkPolyPts.GetAll())-2:
                         
                         connect_pos=Links[-1].AttValue('Length2D')+5
                         
                     elif linkpoly_2_index!=0:
                         
-                        connect_pos=Links[-1].AttValue('Length2D')
+                        connect_pos=Links[-1].AttValue('Length2D')+1
                         
                     else:
                                     
@@ -521,20 +570,58 @@ def Create_connectors(way_type):
         
                         elif way_2.AttValue('Length2D')<5:
                             
-                            connect_pos=3
+                            connect_pos=2
         
                         else:
                             
                             connect_pos=5
-    
+                            
                     Vissim.Net.Links.AddConnector(0,way_1.Lanes.ItemByKey(lane_connection_1), way_1.AttValue('Length2D'), way_2.Lanes.ItemByKey(lane_connection_2), connect_pos, lanes, 'LINESTRING EMPTY')
+                
+                    if linkpoly_2_index!=0:
+                    
+                        Vissim.Net.Links.RemoveLink(Links[-1])
+
+                    break 
+                
+                if linkpoly_2_index!=len(way_2.LinkPolyPts.GetAll())-1 and linkpoly_2_index!=len(way_2.LinkPolyPts.GetAll())-2 and linkpoly_2_index!=1 and linkpoly_2_index!=0 and way_1!=way_2 and int(way_1.AttValue('Name'))!=-int(way_2.AttValue('Name')) and not([int(way_2.AttValue('Name')),int(way_1.AttValue('Name'))] in no_turn_restrictions) and math.dist(way_1_start, [coord_x_2, coord_y_2])<dist:
+
+                    polypoints=way_2.LinkPolyPts.GetAll()
+                    
+                    points_for_link=polypoints[0:linkpoly_2_index+1]
+                    
+                    coords_for_link=[]
+                    
+                    for point in range(len(points_for_link)):
+                        
+                        coords_for_link.append([points_for_link[point].AttValue('X'), points_for_link[point].AttValue('Y')])
+                    
+                    if linkpoly_2_index!=1:
+                
+                        Links.append(
+                            Vissim.Net.Links.AddLink(0, str(sg.LineString(coords_for_link)), [3.5])
+                            )
+                        
+                    lanes=min(way_1.AttValue('NumLanes'), way_2.AttValue('NumLanes'))
+                    
+                    if lanes==1:
+                    
+                        lane_connection_1=1
+                        lane_connection_2=1
+                        
+                    else: 
+                        
+                        lane_connection_1=way_1.AttValue('NumLanes')-lanes+1
+                        lane_connection_2=way_2.AttValue('NumLanes')-lanes+1
+
+                    connect_pos=Links[-1].AttValue('Length2D')-3
+
+                    Vissim.Net.Links.AddConnector(0, way_2.Lanes.ItemByKey(lane_connection_2), connect_pos, way_1.Lanes.ItemByKey(lane_connection_1), 3, lanes, 'LINESTRING EMPTY')
 
                     if linkpoly_2_index!=0:
                     
                         Vissim.Net.Links.RemoveLink(Links[-1])
-                        
-                        
-
+                  
                     break 
 
 print('Creating signal heads')
@@ -600,7 +687,7 @@ for nodes in root:
                                     if link.AttValue('Name')==way_id:
                                 
                                         linkpolypts_1=link.LinkPolyPts.GetAll()
-                                        
+
                                         break
 
                                 for link in Vissim.Net.Links:
@@ -745,23 +832,22 @@ for relations in root:
                     only_restrictions.append([from_way_id, -to_way_id])
                     only_restrictions.append([-from_way_id, -to_way_id])
 
+
 Create_connectors(cycleways)
+
+print('100.0%')
 
 Create_connectors(carways)
 
-Vissim.SaveNetAs(os.path.join(os.getcwd(),"test.inpx"))
+print('100.0%')
+
+Vissim.SaveNetAs(os.path.join(os.getcwd(), str(osm_file)+".inpx"))
 
 done_window=tk.Tk(className='Done')
 
 done_canvas=tk.Canvas(done_window, width=250, height=200)
 done_canvas.pack()
 
-input("You may split links without connection. If you are done, press 'Enter'!" )
-
-Create_connectors(carways)
-
-Vissim.SaveNetAs(os.path.join(os.getcwd(),"test.inpx"))
-
 input("Press 'Enter' to exit")
 
-Vissim.SaveNetAs(os.path.join(os.getcwd(),"test.inpx"))
+Vissim.SaveNetAs(os.path.join(os.getcwd(),str(osm_file)+".inpx"))
